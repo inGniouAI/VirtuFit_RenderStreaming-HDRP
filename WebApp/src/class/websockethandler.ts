6b058adcf682d89e53b5718378a2f56679da66b9
Hetal Chirag Kotadia <hetal.kotadia@ingnious.in>
2022-06-23 15:28:12 +0530

    remove param from unity app url

--- WebApp/src/class/websockethandler.ts
+++ WebApp/src/class/websockethandler.ts
@@ -197,7 +197,7 @@ var exec = require('child_process').execFile;
 
 var RestartUnityApp =function(){
    console.log("fun() start");
-   exec('/Users/hetalchirag/InGnious/VirtuFit_RenderStreaming-HDRP/test.app/Contents/MacOS/VirtuFit_HDRP_RenderStreaming',['--SignalingUrl', Ipport] , function(err, data) {  
+   exec('/Users/hetalchirag/InGnious/VirtuFit_RenderStreaming-HDRP/test.app/Contents/MacOS/VirtuFit_HDRP_RenderStreaming', function(err, data) {  
         console.log(err)
         console.log(data.toString());                       
     });  
